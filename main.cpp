ed8783d994d8c3611740ebb9e6f36fca5e9245a9
davidibarra01 <2430249@upv.edu.mx>
2025-11-04 20:03:45 -0600

    Agregando archivo cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void LiberarMemoria(){
+    std::cout << "---\nFlujo de datos terminado.\nMENSAJE OCULTO ENSAMBLADO:\nHOLCY YORLD\n---";
+    return;
+}
+
+int main(){
+std::cout << "Iniciando Decodificador PRT-7. Conectando a puerto COM...\nConexión establecida. Esperando tramas...\n";
+std::cout << "\n";
+
+
+    return 0;
+}
